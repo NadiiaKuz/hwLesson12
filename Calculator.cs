5e3ebcb8324cdcea77962b446969f5f68e79dea2
Nadiia <nadink1603@gmail.com>
2023-08-30 11:22:28 +0200

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,36 @@
+﻿namespace hwLesson12
+{
+    class Calculator
+    {        
+        public event EventHandler dividedByZero = null;
+
+        public Calculator()
+        {
+            dividedByZero += new EventHandler(Handler);
+        }
+        
+        static private void Handler(object sender, EventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Divided by 0");
+            Console.ResetColor();
+        }
+
+        public void InvokeEvent()
+        {
+            dividedByZero.Invoke(null, new EventArgs());
+        }
+
+        public static double Mul(double a, double b) =>
+                a * b;
+
+        public static double Div(double a, double b) =>
+           a / b;
+
+        public static double Add(double a, double b) =>
+            a + b;
+
+        public static double Sub(double a, double b) =>
+            a - b;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace hwLesson12
+{
+    public delegate double Calculate(double x, double y);
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Input A:");
+                double a = Double.Parse(Console.ReadLine());
+
+                Console.Write("Input B:");
+                double b = Double.Parse(Console.ReadLine());
+
+                Console.Write("Input the sign of the math operation: ");
+                char operation = Char.Parse(Console.ReadLine());
+
+                Calculator calculator = new Calculator();
+
+                if (operation == '/' && b == 0)
+                {
+                    calculator.InvokeEvent();
+                    continue;
+                }
+
+                Calculate calculate = operation switch
+                {
+                    '+' => Calculator.Add,
+                    '-' => Calculator.Sub,
+                    '*' => Calculator.Mul,
+                    '/' => Calculator.Div,
+                };
+
+                Console.WriteLine($"{a} {operation} {b} = {calculate(a, b)}");
+            }
+        }
+    }
+}
